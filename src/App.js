79ce1a8b78a0b56063b0a08899e932e5429cf482
Aleks <68552615+Sentvinsent@users.noreply.github.com>
2024-09-27 20:04:13 +0200

    Added title

--- src/App.js
+++ src/App.js
@@ -4,10 +4,14 @@ import UserForm from "./components/Form/UserForm";
 //CSS
 import "./App.css";
 import List from "./components/List/List";
+import Card from "./components/Card/Card";
 
 function App() {
   return (
     <div className="App">
+      <Card>
+        <h1 style={{ color: '#f0f8ff' }}>USERS LIST</h1>
+      </Card>
       <UserForm />
       <List />
     </div>
